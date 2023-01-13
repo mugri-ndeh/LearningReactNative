e79c2b03ac9b39e24030bd2feb16c149103cec73
mugri-ndeh <frunwindeh@yahoo.com>
2023-01-13 14:24:28 +0100

    view to login

--- App.js
+++ App.js
@@ -1,154 +1,26 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
  *
  * @format
  * @flow strict-local
  */
 
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-  ActivityIndicator,
-} from 'react-native';
+import React from 'react';
+import ReduxToolKitCounterComponent from './src/compoments/learning_components/ReduxToolKitCounterComponent';
+import store from './src/redux/store';
+import { Provider } from 'react-redux';
+import LoginScreen from './src/modules/auth/login/LoginScreen';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({ children, title }) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const [loading, setLoading] = useState(true);
-  const [data, setData] = useState([]);
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  const tick = () => {
-    console.log('load switch ' + loading)
-    setLoading(false)
-    console.log(!loading)
-
-  }
-
-  useEffect(() => {
-    fetchData();
-  }, [])
-
-  const fetchData = () => {
-    setLoading(true)
-    axios.get('https://dummyjson.com/products/').then(res => {
-      setData(res.data['products'])
-      setLoading(false);
-      console.log(data)
-      console.log(loading)
-
-    }).catch((e) => console.log(e))
-  }
-
 
   return (
-    loading ? <View style={styles.loadingIdicator}><ActivityIndicator color={"#000"} /></View> : <SafeAreaView style={backgroundStyle}>
-      {/* <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView> */}
+    <Provider store={store}>
+      <LoginScreen />
+    </Provider>
 
-      <ScrollView>
-        {data.map(e => <Text key={e.id}>{e.id + ' ' + e.description}</Text>)}
-      </ScrollView>
-
-    </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  loadingIdicator: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
-  },
-});
+
 
 export default App;
